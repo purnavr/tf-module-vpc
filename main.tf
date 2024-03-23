83f64d53d374c4f70439948f0c5e432af9ca3090
purna <purnaraomba@gmail.com>
2024-03-23 18:38:39 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "main" {
 ## public_subnets
 resource "aws_subnet" "public_subnets" {
   vpc_id     = aws_vpc.main.id
+
   for_each = var.public_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
@@ -20,9 +21,21 @@ resource "aws_subnet" "public_subnets" {
   )
 }
 
+## public_route table
+resource "aws_route_table" "public_route_tables" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.public_subnets
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-vpc" }
+  )
+}
+
 ## private_subnets
 resource "aws_subnet" "private_subnets" {
   vpc_id     = aws_vpc.main.id
+
   for_each = var.private_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
@@ -30,5 +43,16 @@ resource "aws_subnet" "private_subnets" {
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
+## private_route table
+resource "aws_route_table" "private_route_tables" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.private_subnets
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-vpc" }
   )
 }
