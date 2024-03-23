1722982fdbde88951dcdc81c5789f07aa6b681b8
purna <purnaraomba@gmail.com>
2024-03-23 13:49:46 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ resource "aws_vpc" "main" {
 ### public_subnets
 resource "aws_subnet" "public_subnets" {
   vpc_id     = aws_vpc.main.id
-  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
 
   for_each = var.public_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
+
+  tags = merge(var.tags, { Name = "${var.env}-${each.value["name"]}" })
 
 }
 
