ae6e581240924bbe8bd60098f09eb38035541564
purna <purnaraomba@gmail.com>
2024-03-23 19:21:53 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "public_route_tables" {
 
 resource "aws_route_table_association" "public_association" {
   for_each = var.public_subnets
-  subnet_id      = lookup(lookup(aws_subnet.public_subnets, [each.value["name"]], null), "id", null)
+  subnet_id      = lookup(lookup(aws_subnet.public_subnets, each.value["name"], null), "id", null)
   #subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
   route_table_id = aws_route_table.public_route_tables[each.value["name"]].id
 }
