16bccc268d77d1789366b5c3a04cdae2dd51b040
purna <purnaraomba@gmail.com>
2024-03-23 20:39:58 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -31,6 +31,25 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+# Nat Gateway
+
+resource "aws_eip" "nat" {
+  for_each = var.public_subnets
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "nat-gatways" {
+  for_each = var.public_subnets
+  connectivity_type = "public"
+  allocation_id = aws_eip.nat.[each.value["name"]].id
+  subnet_id     = aws_subnet.public_subnets[each.value["name"]].id
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
+
 ## public_route table
 resource "aws_route_table" "public_route_tables" {
   vpc_id = aws_vpc.main.id
