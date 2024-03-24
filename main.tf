f05bb3449045bb5cbb14d8bb848edf8c83c7462e
purna <purnaraomba@gmail.com>
2024-03-24 11:09:00 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_route_table" "private_route_tables" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gatways["public-${split("-", each.value["name"])[1]}"].id
+    nat_gateway_id = aws_nat_gateway.nat-gatways["public-${split("-",each.value["name"])[1]}"].id
   }
 
   for_each = var.private_subnets
